dce5e21ef0ff5df4d66fa29ca71607a5a7d0492f
kubapi <kuba.sliz@hotmail.com>
2021-06-14 00:46:54 +0200

    deleting space

--- hater/settings.py
+++ hater/settings.py
@@ -31,9 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['hejter-app.herokuapp.com', '127.0.0.1']
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'feed.apps.FeedConfig',
     'django.contrib.admin',
